0374b315875aefb4920d0d44e3c5ba55fc048e7d
Eduardo Costa <eduardocastro.jj@gmail.com>
2015-06-08 17:13:06 -0300

    updated 'NuGet' specification and 'AssemblyInfo' files

--- src/ClassPropertyValidator/Properties/AssemblyInfo.cs
+++ src/ClassPropertyValidator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ClassPropertyValidator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Validation properties (types and their names, enums) from class")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ClassPropertyValidator")]
